e443963e47072f306d88f9dc10b7cdb818ecb07b
Dan Sylvain <dansylvain84@gmail.com>
2020-06-02 12:24:39 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import { Link } from "gatsby"
+import NavbarStyles from "./Navbar.module.css"
+
+import PropTypes from "prop-types"
+import React from "react"
+
+const Navbar = ({ siteTitle }) => (
+  <div className={NavbarStyles}>
+  <div
+    data-collapse="medium"
+    data-animation="default"
+    data-duration={400}
+    role="banner"
+    className="w-nav"
+  />
+  <nav
+    data-collapse="small"
+    data-animation="default"
+    data-duration={400}
+    role="banner"
+    className="navbar w-nav"
+  >
+    <nav role="navigation" className="nav-menu w-nav-menu">
+      <Link to="" className="nav-link w-nav-link">
+        Articles
+      </Link>
+      <Link to="suggest" className="nav-link w-nav-link">
+        SUGGEST
+      </Link>
+      <Link
+        to="subscribe"
+        aria-current="page"
+        className="nav-link w-nav-link w--current"
+      >
+        Subscribe
+      </Link>
+      <Link to="about" className="nav-link w-nav-link">
+        About
+      </Link>
+    </nav>
+    <div className="menu-wrapper w-clearfix w-nav-button">
+      <div className="menu">MENU</div>
+      <div className="menu-icon w-icon-nav-menu" />
+    </div>
+  </nav>
+</div>
+)
+
+Navbar.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Navbar.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Navbar
